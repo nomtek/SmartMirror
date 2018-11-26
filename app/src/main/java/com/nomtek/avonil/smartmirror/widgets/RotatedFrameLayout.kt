750a111121c778b906d0876234b4994ad09f49cd
Krzysztof Krol <k.krol@nomtek.com>
2018-11-26 15:50:13 +0100

    Improve code formatting in RotatedFrameLayout

--- app/src/main/java/com/nomtek/avonil/smartmirror/widgets/RotatedFrameLayout.kt
+++ app/src/main/java/com/nomtek/avonil/smartmirror/widgets/RotatedFrameLayout.kt
@@ -8,16 +8,21 @@ import android.widget.FrameLayout
  * Created by avonil on 9/18/17.
  */
 class RotatedFrameLayout @JvmOverloads constructor(
-        context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+        context: Context,
+        attrs: AttributeSet? = null,
+        defStyleAttr: Int = 0
 ) : FrameLayout(context, attrs, defStyleAttr) {
 
     init {
         rotation = 90f
     }
 
-    override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
-        super.onLayout(changed, l, t, r, b)
-
+    override fun onLayout(changed: Boolean,
+                          left: Int,
+                          top: Int,
+                          right: Int,
+                          bottom: Int) {
+        super.onLayout(changed, left, top, right, bottom)
         val width = width
         val height = height
         val offset = Math.abs(width - height) / 2
@@ -25,9 +30,13 @@ class RotatedFrameLayout @JvmOverloads constructor(
         translationY = offset.toFloat()
     }
 
-    @Synchronized override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+    @Synchronized
+    override fun onMeasure(widthMeasureSpec: Int,
+                           heightMeasureSpec: Int) {
         super.onMeasure(heightMeasureSpec, widthMeasureSpec)
         val offset = Math.abs(measuredWidth - measuredHeight) / 2
-        setMeasuredDimension(measuredHeight, measuredWidth + 2 * offset)
+        val newWidth = measuredHeight
+        val newHeight = measuredWidth + 2 * offset
+        setMeasuredDimension(newWidth, newHeight)
     }
 }
